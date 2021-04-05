b293ee09b59f351a7839d3c89578b2f61f96cffc
likhith sugganahalli <44597096+Likhith-Sugganahalli@users.noreply.github.com>
2021-04-05 11:59:28 +0530

    Add files via upload

---
+++ iot.py
@@ -0,0 +1,40 @@
+from multiprocessing.dummy import Pool
+import time
+import requests
+import sys
+import paho.mqtt.client as mqtt
+import time
+
+def iot_func_callback_sub(client, userdata, message):
+    print("message received " ,str(message.payload.decode("utf-8")))
+    print("message topic=",message.topic)
+    print("message qos=",message.qos)
+    print("message retain flag=",message.retain)
+
+def mqtt_subscribe_thread_start(arg_callback_func, arg_broker_url, arg_broker_port, arg_mqtt_topic, arg_mqtt_qos):
+    try:
+        mqtt_client = mqtt.Client()
+        mqtt_client.on_message = arg_callback_func
+        mqtt_client.connect(arg_broker_url, arg_broker_port)
+        mqtt_client.subscribe(arg_mqtt_topic, arg_mqtt_qos)
+        time.sleep(1) # wait
+        # mqtt_client.loop_forever() # starts a blocking infinite loop
+        mqtt_client.loop_start()    # starts a new thread
+        return 0
+    except:
+        return -1
+
+def mqtt_publish(arg_broker_url, arg_broker_port, arg_mqtt_topic, arg_mqtt_message, arg_mqtt_qos):
+    try:        
+        mqtt_client = mqtt.Client("mqtt_pub")
+        mqtt_client.connect(arg_broker_url, arg_broker_port)
+        mqtt_client.loop_start()
+
+        print("Publishing message to topic", arg_mqtt_topic)
+        mqtt_client.publish(arg_mqtt_topic, arg_mqtt_message, arg_mqtt_qos)
+        time.sleep(0.1) # wait
+
+        mqtt_client.loop_stop() #stop the loop
+        return 0
+    except:
+        return -1
